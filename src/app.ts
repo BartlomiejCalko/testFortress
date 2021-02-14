19a3e0e7636396738886bede06079432e65d3ef1
Bartlomiej Calko <bartlomiej.calko@gmail.com>
2021-02-14 18:53:24 +0100

    html and css view

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+
+const app = document.getElementById("app");
+const p = document.createElement("p");
+const p2 = document.createElement("p");
+p.textContent = "Lekker, unik og moderne toppleilighet med balkong!"; 
+p2.textContent = "Høy standard, 3-soverom, 2-bad.";
+app?.appendChild(p)
+app?.appendChild(p2);
+
+//const kontakt = document.getElementById("kontakt");
+//const pkont = document.createElement("p");
+//pkont.textContent = "Jan-Petter Grønborg,\n 92992929";
+//kontakt?.appendChild(pkont);
+
